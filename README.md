4f7d911422819b8d759e81fbfe80f46ed2859b3f
bgrhnzcn <bugrahanozcan16@gmail.com>
2024-09-27 01:25:59 +0300

    Update GL42Demo.code-workspace, README.md, lib/gl42, shaders/test.glsl, and src/main.cpp

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # GL42Demo
-Demo project for my graphic library.
+### Demo project for my graphic library.
+
+This project is a demonstration of my custom graphic library [gl42](https://github.com/bgrhnzcn/GL42.git) (most likely C++ implementations).
+
+# How to Install
+
+### Clone recursively to clone submodules.
+
+	git clone --recurse-submodule https://github.com/bgrhnzcn/GL42Demo.git
+	cd GL42Demo && ./build.sh
+
+### You can run demo via sript or manuel.
+
+	./run
+or
+
+	build/gl42Demo
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bgrhnzcn <bgrhnzcn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 19:13:46 by buozcan           #+#    #+#             */
-/*   Updated: 2024/09/25 21:07:12 by bgrhnzcn         ###   ########.fr       */
+/*   Updated: 2024/09/27 01:24:49 by bgrhnzcn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,49 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 		std::cout << "Q Released" << std::endl;
 }
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		std::cerr << "Shader compilation failed." << std::endl;
+	}
+
+	return (id);
+}
+
+static unsigned int	CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	//Attaches the shaders to the program.
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	//Links the program.
+	glLinkProgram(program);
+	//Validates the program.
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	return (program);
+}
+
 int main()
 {
 	if (!glfwInit())
 		return (1);
 	try
 	{
-		gl42::Window win = gl42::Window(1920, 1080, "GL42", nullptr, nullptr);
+		gl42::Window win = gl42::Window(800, 600, "GL42", nullptr, nullptr);
 		if (win.getWinPtr() == nullptr)
 		{
 			std::cout << "Window Error" << std::endl;
@@ -45,22 +81,63 @@ int main()
 		}
 		glfwMakeContextCurrent(win.getWinPtr());
 		//buffer id for opengl. OpenGL gives id for every object uses memory segments. Like buffers or arrays.
+
+		GLfloat vertices[6] = {
+			-0.5f, -0.8f,
+			+0.0f, +0.5f,
+			+0.5f, -0.5f
+		};
+		
+		//Generate generic type of buffer and assing it to buffer_id.
 		unsigned int buffer_id;
-		glCreateBuffers(1, &buffer_id);
 		glGenBuffers(1, &buffer_id);
+		//Bind buffer to array buffer. Array buffer is a type of buffer that stores vertex data.
+		glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+		//Copy data to buffer. GL_STATIC_DRAW is a type of draw that tells OpenGL that we are not going to change the data.
+		glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertices, GL_STATIC_DRAW);
+		//Modify vertex attribute array. 0 is the index of the attribute.
+		//2 is the size of the attribute as count. GL_FLOAT is the type of the attribute.
+		//GL_FALSE is the normalization of the attribute. 2 * sizeof(float) is the stride of the attribute.
+		//0 is the offset of the attribute inside single vertex.
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+		//Enables the vertex attribute array.
+		glEnableVertexAttribArray(0);
+		
+		//Vertex Shader
+		std::string ver = 
+			"#version 330 core\n"
+			"\n"
+			"layout(location = 0) in vec4 position;\n"
+			"\n"
+			"void main()\n"
+			"{\n"
+			"	gl_Position = position;\n"
+			"}\n";
+		//Fragment Shader
+		std::string frag = 
+			"#version 330 core\n"
+			"\n"
+			"layout(location = 0) out vec4 color;\n"
+			"\n"
+			"void main()\n"
+			"{\n"
+			"	color = vec4(1, 0, 1, 1);\n"
+			"}\n";
+
+		unsigned int shader = CreateShader(ver, frag);
+		glUseProgram(shader);
+		//Sets Callback function for key events.
+		glfwSetKeyCallback(win.getWinPtr(), key_callback);
+		//Main Rendering loop.
 		while(!win.shouldClose())
 		{
 			//Clear Buffer memeory to remove garbage values
 			glClear(GL_COLOR_BUFFER_BIT);
-			//Legacy opengl version.
-			glBegin(GL_TRIANGLES);
-			glVertex2f(-0.5f, 0.5f);
-			glVertex2f(0.5f, -0.5f);
-			glVertex2f(0.5f, 0.5f);
-			glEnd();
+			//Draws the buffer. GL_TRIANGLES is a type of draw that tells OpenGL to draw triangles.
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 			//Swaps front and back buffer. OpenGL deafult is two buffer. One for back one for front.
 			glfwSwapBuffers(win.getWinPtr());
-			glfwSetKeyCallback(win.getWinPtr(), key_callback);
+			//Polls events. Like key events.
 			glfwPollEvents();
 		}
 	}
