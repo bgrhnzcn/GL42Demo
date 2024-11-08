c9300826b85799737792d992c6cadbcec4924b33
bgrhnzcn <bugrahanozcan16@gmail.com>
2024-11-08 21:47:30 +0300

    Refactor and some debug tools.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-cmake -S . -B build && make -C build/ -j4 2> build/error.log || printf "\e[31mCompiler or Linker Error!\n\e[0m"
+clear
+cmake -S . -B build && make -C build/ -j4 2> build/error.log || (printf "\e[31mCompiler or Linker Error!\n\e[0m" && cat build/error.log)
